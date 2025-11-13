f042dd9be6e82f01a741ae68aa0f2ed1f9a71d30
77aayy <77aayy@gmail.com>
2025-11-13 18:39:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-// server.js
 const jsonServer = require('json-server');
-const cors = require('cors');
-const express = require('express');
+const server = jsonServer.create();
+const path = require('path');
 
-const server = express();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
-server.use(express.json());
 server.use(middlewares);
 
-server.use('/records', router);
+// CORS fix
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+server.use(jsonServer.bodyParser);
+server.use(router);
 
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log('JSON Server is running on port ' + PORT);
 });
